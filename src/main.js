1688e4298719f4d784547d3ca7ac13bd72f800d8
SEONGEUN0415 <wltnr1998@gmail.com>
2022-10-05 22:24:43 +0900

    CSS: Overflow-y + variation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
 'use strict'
 
-var blue__button = document.querySelector('.color__blue');
+const blue__button = document.querySelector('.color__blue');
+const yellow__button = document.querySelector('.color__yellow');
+const pink__button = document.querySelector('.color__pink');
+const category__blue = document.querySelector('.type__blue');
+const category__yellow = document.querySelector('.type__yellow');
+const category__pink = document.querySelector('.type__pink');
+
 blue__button.addEventListener('click', ()=>{
-        console.log('fdkfjdksj');
+    if(category__blue.style.display == 'none'){
+        category__blue.style.display = 'block';
+        category__yellow.style.display = 'none';
+        category__pink.style.display = 'none';
+    }
+});
+
+yellow__button.addEventListener('click', ()=>{
+    if(category__yellow.style.display == 'none'){
+        category__yellow.style.display = 'block';
+        category__pink.style.display = 'none';
+        category__blue.style.display = 'none';
+    } 
+});
+
+pink__button.addEventListener('click', ()=>{
+    if(category__pink.style.display == 'none'){
+        category__pink.style.display = 'block';
+        category__yellow.style.display = 'none';
+        category__blue.style.display = 'none';
+
+    } 
 });
